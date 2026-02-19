a1030ddbb2090ecd4d47c43f0df97729a69d14cf
harshit779921 <harshitpandey072003@gmail.com>
2026-02-20 01:02:27 +0530

    final change

--- config/questions.py
+++ config/questions.py
@@ -128,7 +128,7 @@ confidence_level = "8"             # Any number between "1" to "10" including 1 
 
 ## Allow Manual Inputs
 # Should the tool pause before every submit application during easy apply to let you check the information?
-pause_before_submit = True         # True or False, Note: True or False are case-sensitive
+pause_before_submit = False         # True or False, Note: True or False are case-sensitive
 '''
 Note: Will be treated as False if `run_in_background = True`
 '''
